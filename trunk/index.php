539e2da24ad626bd9135dbfb6c914e509815eee0
luckymancvp <luckymancvp@21aeaa52-3654-20db-8dd6-5cb871740d68>
2011-05-14 09:40:49 +0000

    up tam cai nao

--- trunk/index.php
+++ trunk/index.php
@@ -5,14 +5,58 @@
 <script type="text/javascript" src="js/jquery-1.5.1.min.js"></script>
 <script type="text/javascript" src="js/jquery-ui-1.8.11.custom.min.js"></script>
 <script type="text/javascript" src="js/jquery-ui-timepicker-addon.js"></script>
+<script src="js/js.js"></script>
 <link type="text/css" href="css/ui-lightness/jquery-ui-1.8.11.custom.css" rel="stylesheet" />
+
+<script src="js/JSCal/src/js/jscal2.js"></script>
+<script src="js/JSCal/src/js/lang/en.js"></script>
+<link rel="stylesheet" type="text/css" href="js/JSCal/src/css/jscal2.css" />
+<link rel="stylesheet" type="text/css" href="js/JSCal/src/css/border-radius.css" />
+<link rel="stylesheet" type="text/css" href="js/JSCal/src/css/steel/steel.css" />
+
+<script src="js/toChecklist/jquery.toChecklist.min.js"></script>
+<link rel="stylesheet" type="text/css" href="js/toChecklist/jquery.toChecklist.min.css" />
 
 <style type="text/css">@import "css/global.css";</style>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <script type="text/javascript" src = "map/map.js"></script>
 <script>
+
+	$(document).ready(function () {
+		RANGE_CAL_1 = new Calendar({
+		    inputField: "time-picker-start",
+		    dateFormat: "%d/%m/%Y %H:%M:%S",
+		    trigger: "f_rangeStart_trigger",
+		    bottomBar: false,
+		    showTime   : 24,
+		    onSelect: function() {
+		            var date = Calendar.intToDate(this.selection.get());
+		            this.hide();
+		    }
+		});
+
+		RANGE_CAL_2 = new Calendar({
+		    inputField: "time-picker-end",
+		    dateFormat: "%d/%m/%Y %H:%M:%S",
+		    trigger: "f_rangeEnd_trigger",
+		    bottomBar: false,
+		    showTime   : 24,
+		    onSelect: function() {
+		            var date = Calendar.intToDate(this.selection.get());
+		            this.hide();
+		    }
+		});
+
+		
+	
+	});
+
 	$(function (){		
 		var map = drawSimpleMap();
+		$('#bus-number').toChecklist({
+			addSearchBox            : true,
+			required                : true
+		});
 	});
 
 	count = 0;
@@ -22,10 +66,16 @@
 			1. Display bus way location
 			2. Display bus number info
 		*/
+
+
 		
 		//Get input
-		busNumber  = $("#bus-number").val();
-		timePicker = $("#time-picker").val();
+		busNumber  = $("#bus-number").checklistValue();
+		timePickerStart = $("#time-picker-start").val();
+		timePickerEnd   = $("#time-picker-end").val();
+		for (i=0; i<busNumber.length; i++){
+			alert(busNumber[i]);
+		}
 		
 		//1.Display bus location
 		url = 'buslocation.php?bus_number_plate='+busNumber+'&time_picker='+timePicker;
@@ -55,15 +105,22 @@
 			<div class="side-bar-item-header">
 				BUS
 			</div>
-			<p><label>Chọn xe bus</label></p>
-			<form method="post">
-				<select id="bus-number" name='bus-number'>
+				<p><label>Chọn thời gian </label></p>
+				<input type="text" name="time-picker-start" id="time-picker-start" class="time-picker"/>
+				<button type="button" id="f_rangeStart_trigger" >...</button>
+				<br>
+				<input type="text" name="time-picker-end" id="time-picker-end" class="time-picker"/>				
+				<button type="button" id="f_rangeEnd_trigger" >...</button>
+				
+				<p><label>Chọn Xe</label></p>
+				<form method="post">
+				<select id="bus-number" name='bus-number' multiple="multiple">
 					<?php
 						include_once dirname ( __FILE__ ) . '/config/include.inc.php';
 						/* Get list bus
 						*/
 						$bus = new Bus();
-						$busList = $bus->getBusList();
+						$busList = $bus->getBusNumberPlateList();
 						foreach ($busList as $busitem) {
 							if ($busitem == $_POST['bus-number'])
 								echo "<option selected='selected' value='{$busitem}'>{$busitem}</option>";
@@ -72,13 +129,8 @@
 						}
 					?>
 				</select>
-				<p><label>Chọn thời gian </label></p>
-				<input type="text" name="time-picker" id="time-picker" value="<?php if (isset($_POST['time-picker'])) echo $_POST['time-picker'];?>" />
-				<script type="text/javascript">
-					$("#time-picker").datetimepicker({showSecond: true,
-						timeFormat: 'hh:mm:ss'});
-				</script>
-				<input type="submit" value="Chọn" name='submit'/>
+				<br><br>
+				<input type="button" value="Chọn" name='submit' onclick="javascript:busSelected()"/>
 			</form>	
 		</div>
 		
@@ -92,13 +144,5 @@
 	<div id="map">
 	</div>
 	</div>
-	<?php 
-		if (isset($_POST['submit']))
-			echo "
-				<script>
-					busSelected();
-				</script>
-			";
-	?>
 </body>
 </html>
